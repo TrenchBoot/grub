d2a91184f6cdb7f8fd912e175b8e0a46c2fa0aa7
Ross Philipson <ross.philipson@oracle.com>
2024-03-25 18:34:01 -0700

    slaunch: Add main Secure Launch definitions header
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

---
+++ include/grub/slaunch.h
@@ -0,0 +1,71 @@
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2024, Oracle and/or its affiliates.
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Main secure launch definitions header file.
+ */
+
+#ifndef GRUB_I386_SLAUNCH_H
+#define GRUB_I386_SLAUNCH_H 1
+
+/* Secure launch platform types. */
+#define SLP_NONE	0
+#define SLP_INTEL_TXT	1
+
+#define GRUB_SLAUNCH_TPM_EVT_LOG_SIZE	(8 * GRUB_PAGE_SIZE)
+
+#define SL_PARAM_PREFIX "securelaunch_entry="
+
+#ifndef ASM_FILE
+
+#include <grub/i386/linux.h>
+#include <grub/types.h>
+
+#define GRUB_SL_BOOT_TYPE_INVALID	0
+#define GRUB_SL_BOOT_TYPE_LINUX		1
+#define GRUB_SL_BOOT_TYPE_EFI		2
+
+struct grub_slaunch_params
+{
+  grub_uint32_t boot_type;
+  grub_uint32_t platform_type;
+  struct linux_kernel_params *boot_params;
+  struct grub_relocator *relocator;
+  grub_uint64_t slr_table_base;
+  grub_uint32_t slr_table_size;
+  void *slr_table_mem;
+  grub_uint32_t mle_start;
+  grub_uint32_t mle_size;
+  grub_uint64_t mle_ptab_target;
+  grub_uint32_t mle_ptab_size;
+  void *mle_ptab_mem;
+  grub_uint32_t mle_header_offset;
+  grub_uint32_t ap_wake_block;
+  grub_uint32_t ap_wake_block_size;
+  grub_uint64_t dce_base;
+  grub_uint32_t dce_size;
+  grub_uint64_t tpm_evt_log_base;
+  grub_uint32_t tpm_evt_log_size;
+};
+
+extern grub_uint32_t grub_slaunch_platform_type (void);
+extern void *grub_slaunch_module (void);
+
+void dl_entry(grub_uint64_t dl_ctx);
+
+#endif /* ASM_FILE */
+
+#endif /* GRUB_I386_SLAUNCH_H */
